698c26b3794db865e6a152eb474dabdd032f61c3
Ollie <ohol0007@student.monash.edu>
2025-07-19 16:12:55 +1000

    Initial load of the piece class

--- pieces.py
+++ pieces.py
@@ -99,3 +99,32 @@ tetrominos_colours = TETROMINO_COLORS = {
     'J': (0, 0, 255),
     'L': (255, 165, 0)
 }
+
+class Piece:
+    def __init__(self, shape, x=3, y=0):
+        self.shape = shape
+        self.rotations = tetrominos[shape]
+        self.rotations_index = 0
+        self.matrix = self.rotations[0]
+        self.x = x
+        self.y = y
+        self.colour = tetrominos_colours[shape]
+        self.rotation_count = len(tetrominos[shape])
+    
+    def rotate(self, direction =1):
+        self.rotations_index= (self.rotations_index+direction)% self.rotation_count
+        self.matrix = self.rotations[self.rotations_index]
+    
+    def get_cells(self):
+        "Gives the coordinates occupied by the shape"
+        cells = []
+        for i, row in enumerate(self.matrix):
+            for j, val in enumerate(row):
+                if val:
+                    cells.append(self.y+i, self.x+j)
+        return cells
+    
+    def move(self, dx, dy):
+        self.x += dx
+        self.y +=dy
+    
